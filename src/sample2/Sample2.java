81f295d4e4f502a6289a38949e069b69313f9d36
KingSchoolAccount <marcellones.vellkingjasper@dnsc.edu.ph>
2022-02-10 11:02:35 +0800

    First commit

---
+++ src/sample2/Sample2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sample2;
+
+/**
+ *
+ * @author Velly King Jasper O. Marcellones
+ */
+public class Sample2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("2nd Semd");
+    }
+    
+}
